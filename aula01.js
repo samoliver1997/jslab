88df295e2719d2b729eae5dc8d059d0a96925f9e
Samuel Oliveira <48995406+samoliver1997@users.noreply.github.com>
2021-10-24 20:47:15 -0300

    att

--- aula01.js
+++ aula01.js
@@ -3,5 +3,18 @@ const segundonumero = 2;
 
 const result = primeironumero + segundonumero;
 
-console.log (result);
+console.log(result)
 
+// ponto flutuante
+
+const numeropontoflutuante = 3.3;
+const pontoflutuantesemzero = .5;
+
+const novaoperacao = primeironumero / numeropontoflutuante;
+
+console.log(novaoperacao)
+
+// NaN -> Not a number
+
+const alura = "Alura";
+console.log(alura * primeironumero)
