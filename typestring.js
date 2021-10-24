a028a9d7705660c90da6267f11b78cb86c6cc75e
Samuel Oliveira <48995406+samoliver1997@users.noreply.github.com>
2021-10-24 20:58:15 -0300

    string

---
+++ typestring.js
@@ -0,0 +1,8 @@
+const texto1 = "Teste de string, Ola mundo!!";
+const texto2 = 'Teste de string, Ola mundo!!';
+const senha = "senhauser";
+const stringdenumeros = "324929";
+
+const citacao = 'Sam disse que: "oi mano"';
+console.log(citacao)
+
