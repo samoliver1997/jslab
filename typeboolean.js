69eea42db1808c3debac0c759215cb2fce76c0e5
Samuel Oliveira <48995406+samoliver1997@users.noreply.github.com>
2021-10-24 21:34:39 -0300

    boolean

--- typeboolean.js
+++ typeboolean.js
@@ -1,10 +1,27 @@
-const primeironumero = 5;
-const segundonumero = 10;
+const primeiro_numero = 5;
+const segundo_numero = 10;
 
-console.log(primeironumero === segundonumero);
+console.log(primeiro_numero === segundo_numero);
 
 const texto1 = "Alura";
 const texto2 = "Sam";
 
 console.log(texto1 === texto2);
 
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+let input = null;
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
