efe723b9f5e4bef51f4274c79c687757976b4167
Samuel Oliveira <48995406+samoliver1997@users.noreply.github.com>
2021-10-24 21:21:59 -0300

    boolean

---
+++ typeboolean.js
@@ -0,0 +1,10 @@
+const primeironumero = 5;
+const segundonumero = 10;
+
+console.log(primeironumero === segundonumero);
+
+const texto1 = "Alura";
+const texto2 = "Sam";
+
+console.log(texto1 === texto2);
+
--- typestring.js
+++ typestring.js
@@ -3,6 +3,22 @@ const texto2 = 'Teste de string, Ola mundo!!';
 const senha = "senhauser";
 const stringdenumeros = "324929";
 
-const citacao = 'Sam disse que: "oi mano"';
-console.log(citacao)
+const citacao = 'Meu nome é: ';
+const meunome = "Samuel";
 
+//concatenacao (+)
+
+console.log(citacao + meunome)
+
+//template string ou template literal
+
+//comparando strings case sensitive
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase(); //tratamento de string para minusculo
+
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
